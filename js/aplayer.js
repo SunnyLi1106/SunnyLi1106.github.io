9895efc3b57432dce386e84d9c133c9e967a9a2e
SunnyLi1106 <5327136@qq.com>
2023-11-30 05:45:25 +0000

    Site updated: 2023-11-30 05:45:22

--- js/aplayer.js
+++ js/aplayer.js
@@ -23,11 +23,17 @@ const ap = new APlayer({
         cover: 'http://p2.music.126.net/chqTwmFt5pFV90JW70uHsw==/2533274791300594.jpg',
     }, 
     {
-        name: 'Cloud 9',
-        artist: 'Tobu / Itro',
-        url: 'https://music.163.com/song/media/outer/url?id=29797443.mp3',
-        cover: 'https://p1.music.126.net/m54DG5vHgly4EXFJ5Z6anA==/109951164385282490.jpg',
-    }, 
+        name: 'Visions',
+        artist: 'Acreix',
+        url: 'https://music.163.com/song/media/outer/url?id=34040716.mp3',
+        cover: 'http://p1.music.126.net/FkDHefqpHyhxUdxWFug7mg==/109951165732553232.jpg',
+    },
+    {
+        name: 'Higher',
+        artist: 'Tobu',
+        url: 'https://music.163.com/song/media/outer/url?id=28830412.mp3',
+        cover: 'http://p2.music.126.net/03qQTno02s0z3jcIadX01A==/109951167481007353.jpg',
+    },
     {
         name: 'Skyland',
         artist: 'Mich',
@@ -39,6 +45,30 @@ const ap = new APlayer({
         artist: 'Alex Skrindo',
         url: 'https://music.163.com/song/media/outer/url?id=30064291.mp3',
         cover: 'https://p1.music.126.net/8FArfoyS7o6ofvQX-vMePg==/109951164447628707.jpg',
+    }, 
+    {
+        name: 'Blank',
+        artist: 'Disfigure',
+        url: 'https://music.163.com/song/media/outer/url?id=30512731.mp3',
+        cover: 'http://p2.music.126.net/YZj9FakLAaACx-Gl0skyGg==/109951163439145362.jpg',
+    },
+    {
+        name: '14',
+        artist: 'Aicoiy',
+        url: 'https://music.163.com/song/media/outer/url?id=1856227874.mp3',
+        cover: 'http://p2.music.126.net/FgdzLv9LnJt2OT-4l2kUyw==/109951166119547795.jpg',
+    },
+    {
+        name: 'Link',
+        artist: 'Jim Yosef',
+        url: 'https://music.163.com/song/media/outer/url?id=479422643.mp3',
+        cover: 'http://p2.music.126.net/J6tsIRgQnmM7l9gT7yKUig==/109951163571186586.jpg',
+    },
+    {
+        name: 'Cloud 9',
+        artist: 'Tobu / Itro',
+        url: 'https://music.163.com/song/media/outer/url?id=29797443.mp3',
+        cover: 'https://p1.music.126.net/m54DG5vHgly4EXFJ5Z6anA==/109951164385282490.jpg',
     }, 
     {
         name: 'By My Side',
